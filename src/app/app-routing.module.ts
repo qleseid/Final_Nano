76f5cb415fe48bff5ab477334d6386fb81e937d7
Lucas <qleseid@yahoo.com>
2020-02-03 20:05:15 -0700

    #1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeViewComponent } from './pages/home-view/home-view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
